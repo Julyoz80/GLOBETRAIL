f7cdd266e927ef346d4fed5a06636ce7200b694d
Julie Boulard <juliefm1380@gmail.com>
2025-06-06 13:30:15 +0200

    cloudinary

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,6 +1,6 @@
 class Travel < ApplicationRecord
   belongs_to :user
   has_many :chats, dependent: :destroy
-  
+  has_one_attached :photo
   validates :country, :number_of_travellers, :budget, :trip_duration, presence: true
 end
